8cb73a37827e1e4566ae3ea1cdf2434f2c0ebe0a
Jessica Kalloo <98101353+jessicakalloo@users.noreply.github.com>
2022-03-25 09:35:51 -0400

    Add files via upload

---
+++ shorthand-variable.js
@@ -0,0 +1,18 @@
+var price = 5;
+var quantity = 14;
+var total = price * quantity;
+
+
+var price, quantity, total;
+price = 5;
+quantity = 14;
+total = price * quantity;
+
+
+var price = 5, quantity = 14;
+var total = price * quantity;
+
+
+// Write total into the element with the id of cost
+var el = document.getElementById('cost');
+el.textContent = '$' + total;
